e027dd3effdca88b3c55e9b99d8c2c71a8adcab5
Rafl27 <73622321+Rafl27@users.noreply.github.com>
2022-05-26 21:30:44 -0300

    Update Urna.cs

--- Urna.cs
+++ Urna.cs
@@ -2,38 +2,68 @@ namespace trabalho1POO
 {
     public class Urna
     {
+
+
         static int numeroDeCandidatos = 4, op;
         static Candidato[] candidatos = new Candidato[numeroDeCandidatos];
         string cidadeUrna = "";
         public static void Menu()
         {
 
+            Stream entrada = File.Open("teste1.txt", FileMode.Open);
+            StreamReader leitor = new StreamReader(entrada);
+
             Console.WriteLine("Menu");
             Console.WriteLine("1. Cadastrar eleição");
             Console.WriteLine("2. Adicionar Candidatos");
-            Console.WriteLine("2. ");
             Console.WriteLine("Nº de candidatos");
             numeroDeCandidatos = Convert.ToInt32(Console.ReadLine());
-            op = Convert.ToInt32(Console.ReadLine());
+            string linha = leitor.ReadLine();
+            op = linha;
+
             switch (op)
             {
-                case 1:
+                case "1":
                     AdicionarCandidatos();
                     break;
-                case 2:
+                case "2":
                     cadastrarEleicao();
                     break;
                 default:
                     Console.WriteLine("e aí?");
                     break;
             }
+
         }
 
         public static void cadastrarEleicao()
         {
+            int op2 = Console.ReadLine();
             Console.WriteLine("Tipo de eleição");
             Console.WriteLine("1. Legislativo");
+            //vereador
             Console.WriteLine("2. Executivo");
+            if (op2 == 1)
+            {
+                registraVereador();
+            }
+            else
+            {
+                Console.WriteLine("e ai");
+            }
+        }
+
+        public static void registraVereador()
+        {
+
+            Vereador[] arrayVereadores = new Vereador[5];
+            string linha = leitor.ReadLine();
+            string[] candidato = linha.Split(";");
+            for (int i = 0; i < arrayVereadores.Length; i++)
+            {
+                arrayVereadores[i] = new Vereador(candidato[0], candidato[1], candidato[2], candidato[3], candidato[4]);
+                Console.WriteLine(arrayVereadores[i].nome);
+            }
         }
 
     }
